0b684758441e3ca3d500688acae826350b36bdef
edson-lazaro <edson.lazaro@protecso.com.pe>
2021-02-06 15:38:26 -0500

    update fitness partida

--- middle_game.py
+++ middle_game.py
@@ -1,92 +1,43 @@
 import chess
 import numpy as np
-from Utils.FuncionesEvaluacion.fun_eval import get_all_fun_eval
-from Utils.Games.games_Kasparov import get_games_winner_kasparov
+import pandas as pd
 
-class Middle_Game():
-
-    def __init__(self, game, individual):
-        self.game = game
-        self.individual = individual
-        self.next_moves = []
-        self.board, self.best_move = self.get_board_and_best_move()
-        self.next_boards = self.get_next_boards()
-        self.scores = self.get_scores()
-        self.best_moves_individual = self.get_best_moves_individual()
-
-    def get_board_and_best_move(self):
-        """
-        Get from game the board before turn Kasparov and the best move.
-        """
-        next_games = list(self.game.mainline_moves())
-        size_next_games = len(next_games)
-        game_executed = size_next_games // 2
-
-        if self.game.headers['Black'].find("Kasparov") != -1: 
-            if game_executed % 2 == 0: game_executed -= 1
-            
-        if self.game.headers['White'].find("Kasparov") != -1: 
-            if game_executed % 2 == 1: game_executed -= 1
-
-        new_board = chess.Board()
-
-        for i in range(game_executed):
-            new_board.push(next_games[i])
-
-        aux_board = new_board.copy()
-
-        best_move = aux_board.san(next_games[game_executed])
-
-        return new_board, best_move
-
-    def get_next_boards(self):
-
-        next_boards = []
-        for move in self.board.legal_moves:
-            next_move = self.board.san(move)
-            self.next_moves.append(next_move)
-            aux_board = self.board.copy()
-            aux_board.push_san(next_move)
-            next_boards.append(aux_board)
-        
-        return next_boards
-    
-    def get_scores(self):
-        scores = []
-        for board in self.next_boards:
-            fun_eval = get_all_fun_eval(board)
-            score = np.array(fun_eval) @ np.array(individual)
-            scores.append(score)
-        return scores
-
-    def get_best_moves_individual(self):
-        arr_scores = np.array(self.scores)
-        index_max = list(np.where(arr_scores == arr_scores.max())[0]) 
-        best_moves_individual = [self.next_moves[i] for i in index_max]
-        # print(f'best_moves_individual: {best_moves_individual}')
-        # print(f'best move Kasparov: {self.best_move}')
-        return best_moves_individual
-
-    def good_decision_was_taken(self):
-        try:
-            index = self.best_moves_individual.index(self.best_move)
-            return 1
-        except:
-            return 0
-
-
-def get_fitness_individual(games, individual):
+def get_fitness_individual(df_fun_eval_turn, best_move, individual):
     good_decisions_taken = 0
 
-    for game in games:
-        middle_game = Middle_Game(game, individual)
-        good_decisions_taken += middle_game.good_decision_was_taken()
+    scores = []
+    for i in range(len(df_fun_eval_turn)):
 
-    return good_decisions_taken
+        fun_eval = list(df_fun_eval_turn.iloc[i, 1].split(','))
+        fun_eval = [int(i) for i in fun_eval]
+        score = np.array(fun_eval) @ np.array(individual)
+        scores.append(score)
+
+    arr_scores = np.array(scores)
+    index_max = list(np.where(arr_scores == arr_scores.max())[0]) 
+    next_moves = df_fun_eval_turn.iloc[:,3].values
+    best_moves_individual = next_moves[index_max]
+
+    try:
+        index = best_moves_individual.index(best_move)
+        return 1
+    except:
+        return 0
+    
+def get_best_move(df_games, id_partida):
+    return df_games.iloc[id_partida, 1]
+
+def get_df_fun_eval_partida(df_fun_eval, id_partida):
+    return df_fun_eval.query('id_partida == @id_partida')
 
 if __name__ == "__main__": 
-    games_winner_kasparov = get_games_winner_kasparov()
-    individual = list(np.random.randint(-50, 50, size=33))
-    # middle_game = Middle_Game(games_winner_kasparov[0], individual)
+    df_fun_eval = pd.read_csv('Utils/Data/jugadas_fun_eval.csv', ',', index_col=0)
+    df_games = pd.read_csv('Utils/Data/resumen_jugadas.csv', ',', index_col=0)
+    id_partida = 4
+    
+    best_move = get_best_move(df_games, id_partida)
+    df_fun_eval_turn = get_df_fun_eval_partida(df_fun_eval, id_partida)
+    individual = np.random.randint(10,size=33)
 
-    print(get_fitness_individual(games_winner_kasparov[:10], individual))
+    fitness = get_fitness_individual(df_fun_eval_turn, best_move, individual)
+    print("Fitness: ",fitness)
