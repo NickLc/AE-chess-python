92e048a54b07cba826541473b901abc370e7c7c0
edson-lazaro <edson.lazaro@protecso.com.pe>
2021-02-06 12:03:40 -0500

    actualizar el turno opuesto

--- Utils/FuncionesEvaluacion/fe_nick/getAllFunEval.py
+++ Utils/FuncionesEvaluacion/fe_nick/getAllFunEval.py
@@ -4,14 +4,15 @@ from .Knightperiphery import KnightperipherysAllLevels
 from .MatrixFenBoard import MatrixFenBoard
 from .Pawn import getAllMetricsPawnsBoard
 
-def getTurnBoard(board):
+def getTurnOppositeBoard(board):
     fen = board.fen()
     color = re.findall('\s[bw]\s',fen)[0].replace(' ','')
+    color = 'w' if color=='b' else 'b'
     return color 
 
 def get_all_fun_eval_n(board):
     mfb = MatrixFenBoard(board)
-    color = getTurnBoard(board)
+    color = getTurnOppositeBoard(board)
     metrics_kps = KnightperipherysAllLevels(mfb, color)
     metrics_pawn = getAllMetricsPawnsBoard(mfb, color)
     scores = metrics_kps + metrics_pawn
